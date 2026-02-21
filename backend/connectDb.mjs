bdef54264c2f8d36f9f3ff41f6fd6e7634214a66
AourfiIdir <idir.aourfi@se.univ-bejaia.dz>
2026-02-21 11:28:01 -0500

    /

--- backend/connectDb.mjs
+++ backend/connectDb.mjs
@@ -1,14 +1,14 @@
 import mongoose from "mongoose"
 import "dotenv/config"
 const URL = process.env.DB_URL
-
+if (!URL) throw new Error("DB_URL is not set");
 export const connectDB = async ()=>{
     try{
         await mongoose.connect(URL);
         console.log("Connected to the database successfully");
     }catch(err){
         console.error("Failed to connect to the database");
-        process.exit();
+        process.exit(1);
     }
 }
 
--- backend/server.mjs
+++ backend/server.mjs
@@ -4,13 +4,14 @@ import cors from "cors"
 import {connectDB} from "../backend/connectDb.mjs"
 const app = express();
 const PORT=process.env.PORT;
-const URL=process.env.DB_URL;
 app.use(cors());
 
 
-connectDB(URL).then(()=>{
+connectDB().then(()=>{
 app.listen(PORT,()=>{
     console.log(`app start listening in ${PORT}`);
 })
 })
+
+
 
